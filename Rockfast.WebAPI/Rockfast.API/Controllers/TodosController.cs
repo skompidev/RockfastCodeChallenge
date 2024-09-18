6947188a6d4c52042bf7bafcf9acf62e621492d9
skompidev <38133511+skompidev@users.noreply.github.com>
2024-09-19 04:40:20 +1000

    All changes

--- Rockfast.WebAPI/Rockfast.API/Controllers/TodosController.cs
+++ Rockfast.WebAPI/Rockfast.API/Controllers/TodosController.cs
@@ -16,61 +16,104 @@ namespace Rockfast.API.Controllers
             this._logger = logger;
         }
 
+        /// <summary>
+        /// Get Todos
+        /// </summary>
+        /// <returns>Returns list of todos</returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<TodoVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<IEnumerable<TodoVM>>> Get()
         {
+            this._logger.LogInformation($"Processing Todo {nameof(Get)} request");
             return Ok(await _todoService.Get());
         }
 
+        /// <summary>
+        /// Get Todos by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Todo</returns>
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<TodoVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<TodoVM>>> GetById(int id)
         {
+            this._logger.LogInformation($"Processing Todo {nameof(GetById)} request for {id}");
             return Ok(await _todoService.GetById(id));
         }
 
+        /// <summary>
+        /// Create a Todo
+        /// </summary>
+        /// <param name="model">Todo</param>
+        /// <returns>todo resource</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(IEnumerable<TodoVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<TodoVM>> Post(TodoVM model)
         {
+            this._logger.LogInformation($"Processing Todo {nameof(Post)} request for {model}");
+
             var todo = await _todoService.Post(model);
+
+            this._logger.LogInformation($"Completed Todo {nameof(Post)} request for {todo}");
 
             return Created($"/todos/{todo.Id}", todo);
         }
 
+        /// <summary>
+        /// Updates Todo
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<TodoVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<TodoVM>> Put(TodoVM model)
         {
+            this._logger.LogInformation($"Processing Todo {nameof(Put)} request for {model}");
+
             var todo = await _todoService.Put(model);
+
+            this._logger.LogInformation($"Completed Todo {nameof(Put)} request for {model}");
 
             return Ok(todo);
         }
 
+        /// <summary>
+        /// Delete Todo
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> Delete(int id)
         {
+            this._logger.LogInformation($"Processing Todo {nameof(Delete)} request for {id}");
+
             await _todoService.Delete(id);
 
             return Ok();
         }
 
+        /// <summary>
+        /// Get Todos by User
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>List of Todos for a User</returns>
         [HttpGet("user/{userId:guid}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<TodoVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<TodoVM>>> GetByUserId(Guid userId)
         {
+            this._logger.LogInformation($"Processing Todo {nameof(GetByUserId)} request for {userId}");
+
             return Ok(await _todoService.GetByUserId(userId));
         }
     }
--- Rockfast.WebAPI/Rockfast.API/Controllers/UsersController.cs
+++ Rockfast.WebAPI/Rockfast.API/Controllers/UsersController.cs
@@ -16,50 +16,87 @@ namespace Rockfast.API.Controllers
             this._logger = logger;
         }
 
+        /// <summary>
+        /// Get all users
+        /// </summary>
+        /// <returns>list of users</returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<UserVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<IEnumerable<UserVM>>> Get()
         {
+            this._logger.LogInformation($"Processing User {nameof(Get)} request");
             return Ok(await _userService.Get());
         }
 
+        /// <summary>
+        /// Get user by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>user</returns>
         [HttpGet("{id:guid}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<UserVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<UserVM>>> GetById(Guid id)
         {
+            this._logger.LogInformation($"Processing User {nameof(GetById)} request for {id}");
+
             return Ok(await _userService.GetById(id));
         }
 
+        /// <summary>
+        /// Create a new user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>user</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(IEnumerable<UserVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<UserVM>> Post(UserVM model)
         {
+            this._logger.LogInformation($"Processing User {nameof(Post)} request for {model}");
+
             var user = await _userService.Post(model);
+
+            this._logger.LogInformation($"Completed User {nameof(Post)} request for {model}");
 
             return Created($"/users/{user.Id}", user);
         }
 
+        /// <summary>
+        /// Update user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>user</returns>
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<UserVM>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<UserVM>> Put(UserVM model)
         {
+            this._logger.LogInformation($"Processing User {nameof(Put)} request for {model}");
+
             var user = await _userService.Put(model);
+
+            this._logger.LogInformation($"Completed User {nameof(Put)} request for {model}");
 
             return Ok(user);
         }
 
+        /// <summary>
+        /// Delete user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> Delete(Guid id)
         {
+            this._logger.LogInformation($"Processing User {nameof(Delete)} request for {id}");
+
             await _userService.Delete(id);
 
             return Ok();
